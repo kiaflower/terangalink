f2ff08c71f7d9be22996c34f351ef11cc0a99c89
Kiaaaa <rokhqyq@Kias-Macbook-Pro.local>
2026-05-24 00:36:59 +0000

    Fix Supabase images

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'qeyopmecsgdbnznygvjz.supabase.co.supabase.co',
+      },
+    ],
+  },
+
   eslint: {
     ignoreDuringBuilds: true,
   },
+
   typescript: {
     ignoreBuildErrors: true,
   },
